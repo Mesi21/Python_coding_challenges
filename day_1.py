62f4dabb80a40eeb5a72d1e8e347a8acc69f77c2
Mesi21 <mse212002@gmail.com>
2023-03-13 13:45:30 +0200

    :sparkles: Question 2 solved

--- day_1.py
+++ day_1.py
@@ -8,3 +8,15 @@ for i in range(2000, 3201):
     print(i, end=',')
 
 print("\b")
+
+# Question 2
+# ==========
+# Write a program which can compute the factorial of a given numbers.The results should be printed in a comma-separated sequence
+# on a single line.Suppose the following input is supplied to the program: 8 Then, the output should be:40320
+
+def fact(num):
+  if num == 1 or num == 0:
+    return 1
+  return num*fact(num-1)
+print(fact(8))
+print(fact(int(input())))
