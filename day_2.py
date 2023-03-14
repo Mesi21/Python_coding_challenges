4cdf1cdeaf5f6b3236ef6b8913d9a88856a49216
Mesi21 <mse212002@gmail.com>
2023-03-14 11:18:32 +0200

    :sparkles:  Question 5 solved

---
+++ day_2.py
@@ -0,0 +1,15 @@
+# Question 5
+# ==========
+# Write a program which accepts a sequence of comma-separated numbers from console and generate a list and a tuple which contains 
+# every number.Suppose the following input is supplied to the program:
+# 34,67,55,33,12,98
+# Then, the output should be:
+# ['34', '67', '55', '33', '12', '98']
+# ('34', '67', '55', '33', '12', '98')
+
+my_input = input()
+print(my_input.split(','))
+print(tuple(my_input.split(',')))
+
+# Question 6
+# ==========
