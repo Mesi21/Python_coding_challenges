c63ac10bdd00fb34c05e05640690d0774e9d62e7
Mesi21 <mse212002@gmail.com>
2023-03-20 11:05:14 +0200

    :sparkles: Question 10 solved

---
+++ day_3.py
@@ -0,0 +1,11 @@
+# Question 10
+# ===========
+# Write a program that accepts a sequence of whitespace separated words as input and prints the words 
+# after removing all duplicate words and sorting them alphanumerically.
+
+def sort_words():
+    words = input().split(" ")
+    res = list({item for item in words})
+    return " ".join(sorted(res))
+
+print(sort_words())
