111c0b7c19ad05fdc890dce0d9c4bfeaac6e7afe
Mesi21 <mse212002@gmail.com>
2023-03-27 19:49:03 +0300

    :+1:  Question 12 solved

--- day_3.py
+++ day_3.py
@@ -1,15 +1,16 @@
 # Question 10
 # ===========
-# Write a program that accepts a sequence of whitespace separated words as input and prints the words 
+# Write a program that accepts a sequence of whitespace separated words as input and prints the words
 # after removing all duplicate words and sorting them alphanumerically.
 
-# def sort_words():
-#     words = input().split(" ")
-#     res = list({item for item in words})
-#     return " ".join(sorted(res))
-#
-#
-# print(sort_words())
+def sort_words():
+    words = input().split(" ")
+    res = list({item for item in words})
+    return " ".join(sorted(res))
+
+
+print(sort_words())
+
 
 # Question 11
 # ===========
@@ -19,7 +20,27 @@
 
 def check_div_five():
     nums = input().split(",")
-    res = [num for num in nums if int(num, 2)%5==0]
+    res = [num for num in nums if int(num, 2) % 5 == 0]
     return ','.join(res)
 
+
 print(check_div_five())
+
+# Question 12
+# ===========
+# Write a program, which will find all such numbers between 1000 and 3000 (both included) such that each digit of the
+# number is an even number.The numbers obtained should be printed in a comma-separated sequence on a single line.
+res = []
+for num in range(1000, 3001):
+    count = 0
+    temp = num
+    for r in range(4):
+        r = temp % 10
+        temp = temp // 10
+        if r % 2 == 0:
+            count += 1
+        else:
+            break
+    if count == 4:
+        res.append(num)
+print(','.join([str(num) for num in res]))
